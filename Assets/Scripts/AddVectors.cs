eb18896dbff468874c90b6332a6e8ca0c4390518
srseid <148483902+srseid@users.noreply.github.com>
2025-09-12 12:38:03 -0400

    Add files via upload
    add unity project

---
+++ Assets/Scripts/AddVectors.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AddVectors : MonoBehaviour
+{
+    public Transform redTransform;
+    public Transform blueTransform;
+
+    void Update()
+    {
+        // Exercise: Visualizing Vector Addition
+        // Create a new Vector2 called "rPlusB" which adds rTransform's position to bTransform's position.​
+
+        // When the B key is held down, draw a blue line from the origin to bTransform's position.​
+        // When the R key is held down, draw a red line from the origin to rTransform's position.​
+        // When the R and B keys are both held down, draw a magenta from the origin to rPlusB.​
+
+        // Exercise: Calculating Magnitude
+        // Calculate the magnitude of bPlusR using the mathematical formula for calculating the length of a vector – Pythagorean Theroem.​
+        // Output the magnitude to the console using either Debug.Log or print.
+    }
+}
---
+++ Assets/Scripts/DrawVectors.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DrawVectors : MonoBehaviour
+{
+    void Update()
+    {
+        // Create two Vector2 variables, "dVector" and "eVector"
+        // Set their values to dVector: (0, 1) and eVector: (3, -2)
+
+        // Use Debug.DrawLine to draw a yellow vector starting at the origin and ending at dVector.
+        // Use Debug.DrawLine to draw a grey vector starting at the origin and ending at eVector.
+    }
+}
